d88391e8b23f4cdb6239b683a09c4031fcd70c6d
djshiltz <djs2112@rit.edu>
2021-04-29 18:41:39 -0400

    Updated test.py with plot

--- test.py
+++ test.py
@@ -1,7 +1,24 @@
 from getpass import getpass
 import modtran
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use('Qt5Agg')  # or can use 'TkAgg', whatever you have/prefer
 
+#  Type help(modtran.run) to print documentation
 output = modtran.run(username=input('Enter CIS username: '),
-                     password=getpass(prompt='Enter CIS password: ')
+                     password=getpass(prompt='Enter CIS password: '),
+                     SURREF=0.1,
                      )
 
+# output is a dictionary that includes the tape5 and tape7.scn files, as well
+# as the data columns from tape7.scn
+
+# print(output['tape5'])
+# print(output['tape7.scn]')
+plt.figure(0)
+plt.plot(output['WAVELEN MCRN'], output['TOTAL RAD'], 'k', label='TOTAL RAD')
+plt.plot(output['WAVELEN MCRN'], output['GRND RFLT'], 'b', label='GRND RFLT')
+plt.xlabel(r'Wavelength [$\mu$m]')
+plt.ylabel(r'Radiance [W/cm$^2 \cdot$sr$\cdot \mu$m]')
+plt.legend()
+plt.show()
